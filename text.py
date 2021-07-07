81303732a77be78978ac7a80f32408d9bb28f84a
Jishnu Biju <43580135+Jishnu075@users.noreply.github.com>
2021-07-07 22:25:14 +0530

    Add files via upload

---
+++ text.py
@@ -0,0 +1,31 @@
+from google_trans_new import google_translator  
+import time
+
+
+file_name = open("words.txt","r")
+err_count = 1
+done_file = open("done.txt","w")
+count = 0
+
+for line in file_name:
+    #lists = line.split()
+    text = line  
+    count = count + 1
+    try:
+
+            translator = google_translator()  
+            translate_text = translator.translate(text,lang_tgt='ml')  
+            print(translate_text)
+            done_file.write(translate_text)
+        
+    except Exception:
+            print("couldnt find! , row number ", count)
+
+
+    done_file.write("\n")  
+    err_count = err_count + 1
+    #time.sleep(1)
+
+
+
+
